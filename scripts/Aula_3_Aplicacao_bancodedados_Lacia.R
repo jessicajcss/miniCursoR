a069cc34eb527e33a6cd090aa20ce4b6bc6b5148
jessicajcss <jessica.jcss@ufpr.br>
2025-12-17 15:48:37 -0300

    update Aula 3

--- scripts/Aula_3_Aplicacao_bancodedados_Lacia.R
+++ scripts/Aula_3_Aplicacao_bancodedados_Lacia.R
@@ -162,7 +162,7 @@ dados_nivel <- do.call("bind_rows", myfiles) # aqui, usou-se aplenas o 'myfiles'
 # OPÇÃO 1 – Converter colunas nomeadas
 # Substitua col1, col2 pelos nomes reais das colunas
 
-dados_nivel <- dados_nivel %>%
+dados_nivel <- dados_nivel %>% # Nao rodar!!! apenas um exemplo!
   mutate(
     across(
       c(col1, col2),
@@ -206,6 +206,8 @@ dados_nivel <- dados_nivel %>%
 dados_nivel <- dados_nivel %>%
   mutate(Data = dmy_hms(Data, tz = "America/Sao_Paulo"))
 
+dados_nivel$Data <- as.POSIXct(dados_nivel$Data)
+
 
 ######################################################
 # Parte 3. Data wrangling: Eliminar linhas duplicadas
@@ -217,6 +219,82 @@ dados_estacao <- dados_estacao %>%
 
 dados_nivel <- dados_nivel %>%
   distinct()
+
+
+
+
+
+
+######################################################
+# Parte 4. Data wrangling: Checando os dados
+######################################################
+# Renomeando colunas
+
+# Opção 1:
+dados_estacao_reduzido <- dados_estacao %>%
+  select(Time, Outdoor.Temperature.C., Outdoor.Humidity..., 
+         Wind.m.s., Wind.Direction.deg., Rain.Event.mm., Solar.Rad.w.m2.)
+
+
+colnames(dados_estacao_reduzido) <- c('date', 'temp', 'umid', 'ws', 'wd', 'prec', 'rad')
+
+
+
+# Opção 2:
+
+dados_estacao <- dados_estacao %>%
+  rename(date = Time,
+         ...)
+
+
+# View structure and summary
+glimpse(dados_estacao_reduzido)
+summary(dados_estacao_reduzido)
+
+
+
+# Limpando pelos dados de umidade
+
+dados_estacao_reduzido <- dados_estacao_reduzido  %>%
+  filter(!if_any(everything(), ~ . == 6553.0)) %>%
+  subset(umid <= 100)
+
+# --------------------------------------------------------------------------------------------------------
+#                                   OUTLIER DETECTION AND REMOVAL
+# --------------------------------------------------------------------------------------------------------
+
+# Função para detectar outliers usando percentis
+detect_outlier <- function(x) {
+  
+  # Calcular primeiro e terceiro percentil
+  Percentile1 <- stats::quantile(x, probs = 0.05, na.rm = TRUE)
+  Percentile3 <- stats::quantile(x, probs = 0.95, na.rm = TRUE)
+  
+  # Calcular intervalo interpercentil
+  IPR <- Percentile3 - Percentile1
+  
+  # Retornar verdadeiro para valores que são outliers
+  x > Percentile3 + (IPR * 1.5) | x < Percentile1 - (IPR * 1.5)
+}
+
+# Função para remover outliers de colunas específicas
+remove_outlier <- function(dataframe, columns = names(dataframe)) {
+  dataframe <- dataframe |> dplyr::ungroup() # Remover agrupamento antes da filtragem
+  
+  for (col in columns) {
+    dataframe <- dataframe[!detect_outlier(dataframe[[col]]), ]
+  }
+  
+  return(dataframe) # Retornar o dataframe sem imprimir
+}
+
+
+
+
+df <- dados_estacao_reduzido  %>%
+  remove_outlier(c('temp', 'umid', 'ws', 'wd', 'prec', 'rad'))
+  
+summary(df)
 
 ######################################################
 # Objetos finais:
@@ -245,7 +323,13 @@ dados_nivel <- dados_nivel %>%
 # Parte 4. Visualização
 ######################################################
 
-
+g1 <- dados_estacao %>%
+  ggplot(aes(x = Time, y = Outdoor.Temperature.C.)) +
+  geom_line() +
+  scale_x_date(date_breaks = "month", date_labels = "%b/'%y") +
+  scale_y_continuous(labels = scales::label_comma()) +
+  scale_color_manual(values = c(day = "#FFA200", night = "#757BC7")) +
+  labs(x = NULL, y = "Temperatura média (ºC)")
 
 
 
